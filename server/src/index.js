8170006f07d67964d74a72a75ad3465704607cae
GaddamAkshith <gakshithg@gmail.com>
2025-11-14 19:49:26 +0530

    Fix CORS issue by removing redundant headers

--- server/src/index.js
+++ server/src/index.js
@@ -24,12 +24,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.use(express.json());
 
 const upload = multer({ dest: "uploads/" });
